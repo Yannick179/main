b53628dc80a43c436c5f0ab4d0a6ec3be6d39f00
Yannick Hauffe <134334439+Yannick179@users.noreply.github.com>
2023-10-17 08:20:46 +0000

    implemented algorithm, still missing the counting digit, it still counts the numbers

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,10 +1,12 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
 int get_card(string n);
+int checkluna(string n);
 
 int main(void) 
 {
@@ -21,12 +23,48 @@ int main(void)
             }
         }
     } while (digit == false);
-
-    get_card(number);
+    if (checkluna(number) == 0) {
+        get_card(number);
+    }
+    else {
+        printf("INVALID\n");
+    }
+    
 }
 
 int get_card(string n){
-    if (strlen(n) == 13 && n[0] == '3' && (n[1] == '4' || n[1] == '7')) {
-        printf("yes");
+    if (strlen(n) == 15 && 
+        n[0] == '3' && 
+        (n[1] == '4' || n[1] == '7')) {
+        printf("AMERICAN EXPRESS\n");
     }
+    else if (strlen(n) == 16 && 
+    n[0] == '5' && 
+    (n[1] == '1' || n[1] == '2' || n[1] == '3' || n[1] == '4' || n[1] == '5')) {
+        printf("MASTERCARD\n");
+    }
+    else if ((strlen(n) == 16 || strlen(n) == 13) && n[0] == '4') {
+        printf("VISA\n");
+    }
+    else {
+        printf("INVALID\n");
+    }
+}
+
+int checkluna(string creditcard){
+    int i = 0;
+    int sum = 0;
+    int digitcount = strlen(creditcard);
+    for (int length = strlen(creditcard); digitcount > 0; digitcount -= 1) {
+        int index = digitcount - 1;
+        int index_value = creditcard[index] - '0';
+        if (i % 2 == 0) {
+            sum = sum + index_value;
+        }
+        else {
+            sum = sum + (2*index_value);
+        }
+        i++;
+    }
+    printf("%i\n", sum);
 }
