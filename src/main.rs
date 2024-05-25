dfea8bab79fbcee5c5117bb7f82eb96ffa310daf
Yannick17 <yannickhauffe17@gmail.com>
2024-05-25 10:04:27 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use serde_json::Error;
+
+#[derive(Debug)]
+struct Library {
+    items: Vec<Item>,
+}
+
+fn read_library_file() -> Result<Library, Error> {
+    // Implement function body
+}
+
+fn main() {
+    // Implement function body
+}
